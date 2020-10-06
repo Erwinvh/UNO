4068ed1b736fb870726a56f970d84b252a79dff3
Bart <b.vanbeckhoven1@student.avans.nl>
2020-10-06 14:33:32 +0200

    [CLIENT] Added classes
    Classes added from the class diagram V1

---
+++ UNO/Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UNO
+{
+    public enum Color
+    {
+        RED,
+        BLUE,
+        GREEN,
+        YELLOW,
+        BLACK,
+    }
+}
